0a51e3a068747ab4f75d8cc961be843b624f93d2
https://github.com/kumargarv05-maker <kumargarv05@gmail.com>
2026-09-11 21:46:20 +0530

    understanding the variable in javascript and how to use them

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountID = 12345
+let accountEmail = "garv@google.com"
+var accountPassword = "password#123"
+accountCity = "Ahmedabad"
+let accountState;
+
+// accountID = 134234 // not allow
+
+accountEmail = "thisgk@gk.com"
+accountPassword = "653243#"
+accountCity = "pune"
+
+console.log(accountID)
+
+console.table([ accountEmail, accountID, accountPassword, accountCity, accountState])
+
+
