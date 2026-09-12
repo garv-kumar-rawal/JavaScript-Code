c19340329ad1aed94a39cb78a418c8eed004cbc7
https://github.com/kumargarv05-maker <kumargarv05@gmail.com>
2026-09-12 21:14:30 +0530

    understand the datatypes in javascript

---
+++ 01_basics/02_daatatypes.js
@@ -0,0 +1,27 @@
+
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+console.log( 3
+    + 
+     3
+)   // code redablty is high
+
+
+let name = "garv"
+let age = "20"
+let loogedIn = false
+let state;
+
+//  number -> 2 to power 53
+// BigInt
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined ->
+// Symbol => unique
+
+// object 
+
+console.log(typeof null);
+console.log(typeof undefined);
